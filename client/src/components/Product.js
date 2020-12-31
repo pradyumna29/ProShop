75feaca0b0ded8795067559c80d7c32511341dc2
Pradyumna Gupta <pradyg29@gmail.com>
2020-12-31 12:42:47 +0530

    Rating Component

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
+import Rating from './Rating'
+import PropTypes from 'prop-types'
 
 const Product = ({ product }) => {
   // destructuring to take out product from props
   return (
-    <Card className='my-3 p-3 rounded'>
+    <Card className='my-3 p-2 rounded'>
       <a href={`/product/${product._id}`}>
         <Card.Img src={product.image} variant='top' />
       </a>
@@ -15,14 +17,19 @@ const Product = ({ product }) => {
           </Card.Title>
         </a>
         <Card.Text as='div'>
-          <div className='my-3'>
-            {product.rating} from {product.numReviews} reviews
-          </div>
+          <Rating
+            value={product.rating}
+            text={`${product.numReviews} reviews`}
+          />
         </Card.Text>
         <Card.Text as='h3'>&#x20B9;{product.price}</Card.Text>
       </Card.Body>
     </Card>
   )
+}
+
+Product.propTypes = {
+  product: PropTypes.object.isRequired,
 }
 
 export default Product
