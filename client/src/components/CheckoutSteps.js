fe67d6cf30fc771f68ae0be06c5d93788a76ba89
Pradyumna Gupta <pradyg29@gmail.com>
2021-01-06 12:10:05 +0530

    CheckoutSteps

---
+++ client/src/components/CheckoutSteps.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import { Nav } from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
+
+const CheckoutSteps = ({ step1, step2, step3, step4 }) => {
+  return (
+    <div>
+      <Nav className='justify-content-center mb-4'>
+        <Nav.Item>
+          {step1 ? (
+            <LinkContainer to='/login'>
+              <Nav.Link className='link'>
+                <strong>Sign In</strong>
+              </Nav.Link>
+            </LinkContainer>
+          ) : (
+            <Nav.Link disabled>
+              <strong>Sign In</strong>
+            </Nav.Link>
+          )}
+        </Nav.Item>
+        <Nav.Item>
+          {step2 ? (
+            <LinkContainer to='/shipping'>
+              <Nav.Link className='link'>
+                <strong>Shipping</strong>
+              </Nav.Link>
+            </LinkContainer>
+          ) : (
+            <Nav.Link disabled>
+              <strong>Shipping</strong>
+            </Nav.Link>
+          )}
+        </Nav.Item>
+        <Nav.Item>
+          {step3 ? (
+            <LinkContainer to='/payment'>
+              <Nav.Link className='link'>
+                <strong>Payment</strong>
+              </Nav.Link>
+            </LinkContainer>
+          ) : (
+            <Nav.Link disabled>Payment</Nav.Link>
+          )}
+        </Nav.Item>
+        <Nav.Item>
+          {step4 ? (
+            <LinkContainer to='/placeorder'>
+              <Nav.Link className='link'>
+                <strong>Place Order</strong>
+              </Nav.Link>
+            </LinkContainer>
+          ) : (
+            <Nav.Link disabled>Place Order</Nav.Link>
+          )}
+        </Nav.Item>
+      </Nav>
+    </div>
+  )
+}
+
+export default CheckoutSteps
